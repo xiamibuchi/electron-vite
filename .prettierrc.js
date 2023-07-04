51db53d7206a236870f2e8819b55d40f394ed07f
shenyang <shenyang@xd.com>
2023-07-04 20:32:08 +0800

    feat: init electron

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  bracketSpacing: true,
+};
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+const theme = ref('System');
+
+const toggleTheme = async () => {
+  // @ts-ignore
+  const isDarkMode = await window.darkMode.toggle();
+  theme.value = isDarkMode ? 'Dark' : 'Light';
+};
+
+const resetSystem = async () => {
+  // @ts-ignore
+  await window.darkMode.system();
+  theme.value = 'System';
+};
+
+onMounted(() => {
+  window.postMessage('removeLoading', '*');
+});
+console.log(`Hello world from Electron!`);
+</script>
+
+<template>
+  <p>
+    Current theme source: <strong id="theme-source">{{ theme }}</strong>
+  </p>
+  <button @click="toggleTheme">Toggle Dark</button><br />
+  <button @click="resetSystem">Reset to System Theme</button>
+</template>
